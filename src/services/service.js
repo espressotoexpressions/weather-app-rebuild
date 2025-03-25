bac8cbd5551d892426189c065beb929db680726e
espressotoexpressions <keannevillar@gmail.com>
2025-03-25 15:50:23 -0700

    removed uneccessary code in services

--- src/services/service.js
+++ src/services/service.js
@@ -48,40 +48,7 @@ export const get5DayForecastData= async(latitude,longitude)=>{
       
     return data;
 
-        //forecast data population in UI
-        for (let i=0; i<5;i++)// because we only want the top 5 entries of the returned json data
-        {
-    
-            let varDayName =   `forecast${i}Day`;
-            let iconName = `forecast${i}Icon`;
-            let forecastWeatherName =   `forecast${i}weather`;
-            let forecastMaxName = `forecast${i}max`;
-            let forecastMinName = `forecast${i}min`; 
-            
-            let forecastDayElement = document.getElementById(varDayName.toString());
-            let forecastIconElement = document.getElementById(iconName.toString());
-            let forecastWeatherElement = document.getElementById(forecastWeatherName.toString());
-            let forecastMaxElement = document.getElementById(forecastMaxName.toString());
-            let forecastMinElement = document.getElementById(forecastMinName.toString());
-         
-          
-             const timestampforecast= data.list[i*8].dt; // multplied index to 8 to account for each day because there are 8 entries of 3 hr intervals each day
-            // Convert the timestamp to a JavaScript Date object
-            const date = new Date(timestampforecast * 1000); 
-    
-            // Display the formatted date to show Day only
-            forecastDayElement.innerText=(date.toLocaleString('en-US', {
-                weekday: 'short'
-            })).toUpperCase(); 
-                    
-          
-    ;        //icon mapping from weather api
-            let iconCode = data.list[i*8].weather[0].icon; // accesss first weather condition as it is the primary if multiple weather condition is returned
-            forecastIconElement.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-            forecastWeatherElement.innerText =data.list[i*8].weather[0].description;
-            forecastMaxElement.innerText=`${data.list[i*8].main.temp_max}°F`;
-            forecastMinElement.innerText=`${data.list[i*8].main.temp_min}°F`;
-        }
+      
     
      }
  
